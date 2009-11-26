3a4a9278d885f169549e8e46ce532a91ea1644d1
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2009-11-26 15:23:09 +0100

    Get serial port from bios data area.

--- standalone/serial.c
+++ standalone/serial.c
@@ -1,6 +1,8 @@
 /* From Nova. GPL'd */
 #include <asm.h>
 #include <serial.h>
+#include <bda.h>
+#include <util.h>
 
 enum Port  {
   RHR         = 0,    // Receive Holding Register     (read)
@@ -22,17 +24,17 @@ enum {
   IER_THR_INTR        = 1u << 1,  // Transmit Holding Register Interrupt
   IER_RLS_INTR        = 1u << 2,  // Receive Line Status Interrupt
   IER_MOS_INTR        = 1u << 3,  // Modem Status Interrupt
-  
+
   FCR_FIFO_ENABLE     = 1u << 0,  // FIFO Enable
   FCR_RECV_FIFO_RESET = 1u << 1,  // Receiver FIFO Reset
   FCR_TMIT_FIFO_RESET = 1u << 2,  // Transmit FIFO Reset
   FCR_DMA_MODE        = 1u << 3,  // DMA Mode Select
   FCR_RECV_TRIG_LOW   = 1u << 6,  // Receiver Trigger LSB
   FCR_RECV_TRIG_HIGH  = 1u << 7,  // Receiver Trigger MSB
-  
+
   ISR_INTR_STATUS     = 1u << 0,  // Interrupt Status
   ISR_INTR_PRIO       = 7u << 1,  // Interrupt Priority
-  
+
   LCR_DATA_BITS_5     = 0u << 0,
   LCR_DATA_BITS_6     = 1u << 0,
   LCR_DATA_BITS_7     = 2u << 0,
@@ -44,13 +46,13 @@ enum {
   LCR_PARITY_FORCE    = 1u << 5,
   LCR_BREAK           = 1u << 6,
   LCR_DLAB            = 1u << 7,
-  
+
   MCR_DTR             = 1u << 0,  // Data Terminal Ready
   MCR_RTS             = 1u << 1,  // Request To Send
   MCR_GPO_1           = 1u << 2,  // General Purpose Output 1
   MCR_GPO_2           = 1u << 3,  // General Purpose Output 2
   MCR_LOOP            = 1u << 4,  // Loopback Check
-  
+
   LSR_RECV_READY      = 1u << 0,
   LSR_OVERRUN_ERROR   = 1u << 1,
   LSR_PARITY_ERROR    = 1u << 2,
@@ -60,36 +62,43 @@ enum {
   LSR_TMIT_EMPTY      = 1u << 6
 };
 
-#define SERIAL_BASE 0x3f8
+static uint16_t serial_base;
 
 void
 serial_send (int c)
 {
-  while (!(inb (SERIAL_BASE + LSR) & LSR_TMIT_HOLD_EMPTY)) {
+  while (!(inb (serial_base + LSR) & LSR_TMIT_HOLD_EMPTY)) {
     asm volatile ("pause");
   }
-  
-  outb (SERIAL_BASE + THR, c);
+
+  outb (serial_base + THR, c);
 }
 
 void
 serial_init()
 {
+  /* Disable output if there is no serial port. */
+  /* XXX This is disabled, because it is not reliable. */
+  //output_enabled = (serial_ports(get_bios_data_area()) > 0);
+
+  /* Get our port from the BIOS data area. */
+  serial_base = get_bios_data_area()->com_port[0];
+
   /* Programming the first serial adapter (8N1) */
-  outb (SERIAL_BASE + LCR, LCR_DLAB);
-
+  outb (serial_base + LCR, LCR_DLAB);
+  
   /* 115200 baud */
-  outb (SERIAL_BASE + DLR_LOW,  1);
-  outb (SERIAL_BASE + DLR_HIGH, 0);
-
+  outb (serial_base + DLR_LOW,  1);
+  outb (serial_base + DLR_HIGH, 0);
+    
   /* 9600 baud */
-/*   outb (SERIAL_BASE + DLR_LOW,  0x0C); */
-/*   outb (SERIAL_BASE + DLR_HIGH, 0); */
-
-  outb (SERIAL_BASE + LCR, LCR_DATA_BITS_8 | LCR_STOP_BITS_1);
-  outb (SERIAL_BASE + IER, 0);
-  outb (SERIAL_BASE + FCR, FCR_FIFO_ENABLE | FCR_RECV_FIFO_RESET | FCR_TMIT_FIFO_RESET);
-  outb (SERIAL_BASE + MCR, MCR_DTR | MCR_RTS);
+  /*   outb (serial_base + DLR_LOW,  0x0C); */
+  /*   outb (serial_base + DLR_HIGH, 0); */
+  
+  outb (serial_base + LCR, LCR_DATA_BITS_8 | LCR_STOP_BITS_1);
+  outb (serial_base + IER, 0);
+  outb (serial_base + FCR, FCR_FIFO_ENABLE | FCR_RECV_FIFO_RESET | FCR_TMIT_FIFO_RESET);
+  outb (serial_base + MCR, MCR_DTR | MCR_RTS);
 }
 
 /* EOF */
