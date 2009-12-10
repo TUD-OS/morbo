b75da03b0561b6c23d29230870bf470e4b2f53a1
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2009-12-10 17:58:12 +0100

    Port fw_peek back to libraw1394. The other tools are disconnected from the build for now.

--- tools/fw_peek.cpp
+++ tools/fw_peek.cpp
@@ -2,42 +2,96 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cinttypes>
+#include <cstdint>
 
 #include <getopt.h>
 #include <unistd.h>
 
-#include <brimstone.h>
+#include <arpa/inet.h>
 
-using namespace Brimstone;
+#include <libraw1394/raw1394.h>
+#include <libraw1394/csr.h>
 
-static char usage[] = "Usage: %s node-dev addr\n";
+#include <ohci-constants.h>
+
+static char usage[] = "Usage: %s [-q] [-p port] guid/nodeno address length\n";
 
 int
 main(int argc, char **argv)
 {
   /* Command line parsing */
   int opt;
+  unsigned port = 0;
+  bool quadletwise = false;
 
-  while ((opt = getopt(argc, argv, "")) != -1) {
+  while ((opt = getopt(argc, argv, "qp:")) != -1) {
     switch (opt) {
+    case 'q':
+      quadletwise = true;
+      break;
+    case 'p':
+      port = atoi(optarg);
+      break;
     default:
       fprintf(stderr, usage, argv[0]);
       exit(EXIT_FAILURE);
     }
   }
 
-  if ((argc - optind) != 2) {
+  if ((argc - optind) != 3) {
     fprintf(stderr, usage, argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  /* Connect to Firewire device. */
-  Node node(argv[optind]);
+  uint64_t guid    = strtoull(argv[optind],     NULL, 0);
+  uint64_t address = strtoull(argv[optind + 1], NULL, 0);
+  uint64_t length  = strtoull(argv[optind + 2], NULL, 0);
 
-  // printf("%x\n", node.quadlet_read(strtoll(argv[optind+1], NULL, 0)));
-  for (uint32_t cur = 0; cur < (1<<16); cur += 4) {
-    uint32_t data = node.quadlet_read(cur + 0xC0000);
-    write(STDOUT_FILENO, &data, 4);
+  raw1394handle_t fw_handle = raw1394_new_handle_on_port(port);
+
+  if (fw_handle == NULL) {
+    perror("raw1394_new_handle_on_port");
+    exit(EXIT_FAILURE);
+  }
+
+  nodeid_t target;
+
+  // 63 is broadcast. Ignore that.
+  if (guid < 63) {
+    // GUID is actually a node number.
+    target = LOCAL_BUS | (nodeid_t)guid;
+  } else {
+    for (unsigned no = 0; no < 63; no++) {
+      nodeid_t test_node = LOCAL_BUS | (nodeid_t)no;
+      uint32_t guid_hi;
+      uint32_t guid_lo;
+
+      int res = raw1394_read(fw_handle, test_node, CSR_REGISTER_BASE + CSR_CONFIG_ROM + 4*4,
+			     4, &guid_lo);
+      if (res != 0) { perror("read guid_lo"); return -1; }
+      res = raw1394_read(fw_handle, test_node, CSR_REGISTER_BASE + CSR_CONFIG_ROM + 3*4,
+			 4, &guid_hi);
+      if (res != 0) { perror("read guid_hi"); return -1; }
+
+      uint64_t test_guid = (uint64_t)ntohl(guid_hi) << 32 | ntohl(guid_lo);
+      if (test_guid == guid) { 
+	target = test_node;
+	goto target_found;
+      }
+    }
+    return -1;
+  target_found:
+    ;
+  }
+
+  unsigned step = quadletwise ? 4 : 512;
+  for (uint64_t cur = address; cur < address+length; cur += step) {
+    quadlet_t buf[step/sizeof(quadlet_t)];
+    size_t size = (cur + step > address+length) ? (address+length - cur) : step;
+    int res = raw1394_read(fw_handle, target, cur, size, buf);
+    if (res != 0) { perror("read data"); return -1; }
+    write(STDOUT_FILENO, buf, size);
   }
 
   return 0;
