34e4af08bb7b8ee8f6872893f633e6197a19fbf1
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2009-12-11 17:58:38 +0100

    Adapted RemoteFw to use fw_{peek,poke}.

--- boot/firewire.py
+++ boot/firewire.py
@@ -1,16 +1,13 @@
 import os, struct
 
 class RemoteFw:
-    def __init__(self):
-        self.node = 0
-        self.bs = 2048
+    def __init__(self, node = 0):
+        self.node = node
     def read(self, address, count):
-        bs = self.bs < count and self.bs or count
-        pipe = os.popen("fwcat -n %d -r -a %#x -s %#x -c %#x"%(self.node, address, bs, count/bs), "r")
+        pipe = os.popen("fw_peek %d 0x%08x0x%08x" % (self.node, address, count), "r")
         return pipe.read()        
     def write(self, address, value):
-        bs = self.bs < len(value) and self.bs or len(value)
-        pipe = os.popen("fwcat -n %d -w -a %#x -s %#x -c %#x"%(self.node, address, bs, (len(value)+bs-1)/bs), "w")
+        pipe = os.popen("fw_poke %d 0x%08x" % (self.node, address), "w")
         pipe.write(value)
     def send_init(self):
         msg = struct.pack("I", 0x500)
