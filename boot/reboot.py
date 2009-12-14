929eb18f8feee6bacc3de65fd4341b2e27419d9a
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2009-12-14 15:37:10 +0100

    reboot.py now accepts a kernel binary as parameter.

--- boot/reboot.py
+++ boot/reboot.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 """reboot the remote system"""
 
-import struct, os, config, binary
+import struct, os, sys, binary
 
-def reboot_nova(fw):
+def reboot_nova(kernel, fw):
     """rebooting a running nova is hard: we modify the NMI vector in
     the IDT to point to the ACPI-reset routine and send an NMI."""
-    b = binary.Binary(config.PATHS["hypervisor"])
+    b = binary.Binary(kernel)
     idt = b.get_symbol_phys("_ZN3Idt3idtE")
     acpi_reset = b.get_symbol("_ZN4Acpi5resetEv")
     desc= [acpi_reset & 0xffff, 0x0008, 0x8e00, (acpi_reset >> 16) & 0xffff]
     fw.write(idt + 2*0x8, struct.pack("H"*4,*desc))
     fw.send_nmi()
 
-def reboot(fw=None):
+def reboot(kernel, fw=None):
     if not fw:
         import firewire    
         fw = firewire.RemoteFw()
     fw.send_init()
-    reboot_nova(fw)
+    reboot_nova(kernel, fw)
     
 if __name__ == "__main__":
-    reboot()
+    reboot(sys.argv[1])
