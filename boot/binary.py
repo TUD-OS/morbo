008258107427d20e9473016ea3468343c53d1b67
Bernhard Kauer <kauer@os.inf.tu-dresden.de>
2009-12-30 16:07:33 +0100

    also parse entry point and provide elf decoding

--- boot/binary.py
+++ boot/binary.py
@@ -13,19 +13,25 @@ class Binary:
         for line in os.popen("readelf -l %s"%self.name).readlines():
             line = line[:-1].strip()
             if line.startswith("LOAD"):
-                x = line.split()
-                self.regions.append((int(x[2], 16),int(x[3], 16), int(x[5], 16)))
-
+                self.regions.append(map(lambda x: int(x, 0), line.split()[1:6]))
+            if line.startswith("Entry point"):
+                self.entry_point = int(line.split()[-1], 0)
     def get_symbol(self, name):
         "return the symbol"
         return self.symbols[name]
     def get_phys(self, addr):
-        for virt,phys,length in self.regions:
-            if addr >= virt and addr < virt+length:
+        "get the physical address of a symbol from the program headers"
+        for ofs,virt,phys,fsize,msize in self.regions:
+            if addr >= virt and addr < virt + msize:
                 return addr - virt + phys
-        
-
     def get_symbol_phys(self, name):
         "get the physical address of a symbol"
         return self.get_phys(self.get_symbol(name))
-
+    def decode(self, fw):
+        "elf decode the binary"
+        f = open(self.name)
+        for ofs,virt,phys,fsize,msize in self.regions:
+            f.seek(ofs)
+            fw.write(phys, f.read(fsize))
+            fw.write(phys + fsize, "\x00"*(msize - fsize))
+        return self.entry_point
