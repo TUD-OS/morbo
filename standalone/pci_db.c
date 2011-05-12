484926085b54945dbe105e7764c93937c600a6f0
Julian Stecklina <js@alien8.de>
2011-05-12 13:07:56 +0200

    Added PCI Express card to list of known devices.

--- standalone/pci_db.c
+++ standalone/pci_db.c
@@ -9,6 +9,7 @@
 static const struct pci_db_entry vendor_db[] = {
   /* Texas Instruments */
   { 0x104c,   0x8023,   NO_QUIRKS, "Texas Instruments IEEE1394a-2000 OHCI PHY/Link-Layer Ctrlr" },
+  { 0x104c,   0x8235,   NO_QUIRKS, "Texas Instruments XIO2200(A) IEEE-1394a-2000 Controller (PHY/Link)" },
   { 0x104c,   WILDCARD, NO_QUIRKS, "Texas Instruments Unknown Device" },
   /* NEC */
   { 0x1033,   0x00e7,   NO_QUIRKS, "NEC Electronics IEEE1394 OHCI 1.1 2-port PHY-Link Ctrlr" },
