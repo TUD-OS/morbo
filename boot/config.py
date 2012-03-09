bad864710245f6cca91288f2229beae8305548c7
Julian Stecklina <js@alien8.de>
2012-03-09 16:29:29 +0100

    Adapt config.

--- boot/config.py
+++ boot/config.py
@@ -1,5 +1,5 @@
 import os
-PATHS = {"hypervisor": "~/boot/nova/hypervisor",
+PATHS = {"hypervisor": "~/boot/nul/hypervisor",
          "bootdir":    os.path.expanduser("~/boot/"),
 	}
 PROGS = {"fwread" : "fw_peek %(node)d %(address)#8x %(count)#8x"}
